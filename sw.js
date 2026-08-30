a9b358182e4a70a212847f43a387b034c72855e6
TorbenYde <yde.torben@gmail.com>
2026-08-30 19:07:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE="superyatzy-shell-v21";
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.add("./index.html")));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match(event.request))
+  );
+});
